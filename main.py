dc8b443144e88b291bd7e7604fb61e004ae04abc
thedredhorseman <dredtherapper@cock.li>
2024-09-28 03:49:05 -0700

    Starting fresh

---
+++ main.py
@@ -0,0 +1,5 @@
+import torf
+
+# TODO: function to handle deployment of site; Should take contents of directory; create or update the magnent link and then submit it to a DHT
+def handle_deployment ()
+    
